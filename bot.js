4350b9b104ff8f8926259428be53631dfd1de093
Leon Magnus <dalil.dig@gmail.com>
2017-09-09 20:24:34 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,19 @@
 const Discord = require("discord.js");
 const bot = new Discord.Client();
+//base de donne
+var pg = require('pg');
 
+pg.defaults.ssl = true;
+pg.connect(process.env.DATABASE_URL, function(err, client) {
+  if (err) throw err;
+  console.log('Connected to postgres! Getting schemas...');
+
+  client
+    .query('SELECT table_schema,table_name FROM information_schema.tables;')
+    .on('row', function(row) {
+      console.log(JSON.stringify(row));
+    });
+});
 
 //fichier ou enrigistre les lvl 
 var lvl = require("./lvl.json");
