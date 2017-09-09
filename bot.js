5d7f8d91b8f8413663835cf7f0beeafc487a36bd
Leon Magnus <dalil.dig@gmail.com>
2017-09-09 20:26:20 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,11 +8,11 @@ pg.connect(process.env.DATABASE_URL, function(err, client) {
   if (err) throw err;
   console.log('Connected to postgres! Getting schemas...');
 
-  client
+ /*client
     .query('SELECT table_schema,table_name FROM information_schema.tables;')
     .on('row', function(row) {
       console.log(JSON.stringify(row));
-    });
+    });*/
 });
 
 //fichier ou enrigistre les lvl 
